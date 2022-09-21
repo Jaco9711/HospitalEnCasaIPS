eadf92fb842c9a9d699b3aa5e30388b26308ec8f
Jaco9711 <jaco9711@gmail.com>
2022-09-20 21:03:44 -0500

    configuracion vue

--- HospitalEnCasaFe/hospitalencasafe/src/views/HomeView.vue
+++ HospitalEnCasaFe/hospitalencasafe/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Configuración inicial HOS-G59-60"/>
   </div>
 </template>
 

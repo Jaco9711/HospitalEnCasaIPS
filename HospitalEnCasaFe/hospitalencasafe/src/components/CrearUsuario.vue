c6bcc3bdbebac35e1f304e3ad242359a67f5b719
Jaco9711 <jaco9711@gmail.com>
2022-09-25 18:52:14 -0500

    Crear Usuario

--- HospitalEnCasaFe/hospitalencasafe/src/components/CrearUsuario.vue
+++ HospitalEnCasaFe/hospitalencasafe/src/components/CrearUsuario.vue
@@ -1,3 +1,41 @@
 <template>
     <h1>Crear Usuario</h1>
+    <div>
+        <form>
+            <label>
+                Username:
+            </label>
+            <input type="text" placeholder="Username">
+            <br>
+            <label>
+                Password:
+            </label>
+            <input type="Password">  
+            <br>     
+            <label>
+                Nombres:
+            </label>
+            <input type="text" placeholder="Nombres">                 
+            <br>               
+            <label>
+                Apellidos:
+            </label>
+            <input type="text" placeholder="Apellidos">            
+            <br>   
+            <label>
+                Telefono:
+            </label>
+            <input type="text" placeholder="Telefono">            
+            <br>               
+            <label>
+                Genero:
+            </label>
+            <select id="u_genero">
+                <option>Masculino</option>
+                <option>Femenino</option>
+            </select>     
+            <br>
+            <button>Registrar</button>    
+        </form>
+    </div>
 </template>

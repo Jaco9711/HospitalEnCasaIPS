cfd79bd38e69208a9d57aca61038de6aa58faa5c
Jaco9711 <jaco9711@gmail.com>
2022-09-25 22:48:11 -0500

    style

--- HospitalEnCasaFe/hospitalencasafe/src/App.vue
+++ HospitalEnCasaFe/hospitalencasafe/src/App.vue
@@ -4,14 +4,16 @@
     <router-link to="/rps">Registrar PersonalSalud</router-link>|
     <router-link to="/rp">Registrar Paciente</router-link>|
     <router-link to="/rf">Registrar Familiar</router-link>|    
-    <router-link to="/consPaciente">Consultar Paciente</router-link>
-    
+    <router-link to="/consPaciente">Consultar Paciente</router-link>    
   </nav>
   <router-view/>
 
 </template>
 
 <style>
+body{
+    background-color: #7e7e7e21;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -32,4 +34,15 @@ nav a {
 nav a.router-link-exact-active {
   color: #42b983;
 }
+/*.container_RegistroPersonalSalud{
+        border: 3px solid #283747;
+        border-radius: 10px;
+        width: 25%;
+        height: 85%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        box-shadow: 9px 7px 28px 12px rgba(0,0,0,0.42);
+    }*/
 </style>
